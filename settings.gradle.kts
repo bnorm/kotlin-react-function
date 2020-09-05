08b2ac9eef030ba2cb1ad52fe57a28b8c809a5cf
Brian Norman <brian@bnorm.com>
2020-09-05 15:46:04 -0500

    Don't include kotlin-compile-testing until new version released

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,3 @@ rootProject.name = "kotlin-react-function"
 include(":kotlin-react-function")
 include(":kotlin-react-function-gradle")
 include(":kotlin-react-function-plugin")
-
-includeBuild("../kotlin-compile-testing")
