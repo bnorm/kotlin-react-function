77065d094d3e363383d30a43181e79b044a0929d
Brian Norman <brian@bnorm.com>
2020-09-05 16:23:29 -0500

    Include link to live sample project

--- sample/README.md
+++ sample/README.md
@@ -1,4 +1,5 @@
 # Sample Project
 
 Kotlin/JS based version of [todometer](https://github.com/cassidoo/todometer)
-by [Cassidy Williams](https://github.com/cassidoo).
+by [Cassidy Williams](https://github.com/cassidoo). Available live
+[here](https://bnorm.github.io/kotlin-react-function/).
--- sample/src/main/kotlin/com/bnorm/react/storage.kt
+++ sample/src/main/kotlin/com/bnorm/react/storage.kt
@@ -3,9 +3,11 @@ package com.bnorm.react
 import kotlinx.browser.localStorage
 import kotlinx.serialization.json.Json
 
+private const val KEY = "com.bnorm.react.kotlin-react-function-sample.state"
+
 fun loadState(): AppState {
   try {
-    val serializedState = localStorage.getItem("state") ?: return AppState()
+    val serializedState = localStorage.getItem(KEY) ?: return AppState()
     return Json.decodeFromString(AppState.serializer(), serializedState)
   } catch (t: Throwable) {
     return AppState()
@@ -15,7 +17,7 @@ fun loadState(): AppState {
 fun saveState(state: AppState) {
   try {
     val serializedState = Json.encodeToString(AppState.serializer(), state)
-    localStorage.setItem("state", serializedState)
+    localStorage.setItem(KEY, serializedState)
   } catch (t: Throwable) {
     console.error(t)
   }
