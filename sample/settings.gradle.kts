c0fcd6b16fe1425ff42e8564c46c79bedcb9ffda
Brian Norman <brian@bnorm.com>
2020-10-05 20:46:45 -0500

    Dependency version updates

--- sample/settings.gradle.kts
+++ sample/settings.gradle.kts
@@ -1,10 +1,4 @@
 rootProject.name = "kotlin-react-function-sample"
 
 // To test this sample project against changes to the compiler plugin, uncomment the following line
-includeBuild("..") {
-  dependencySubstitution {
-    substitute(module("com.bnorm.react:kotlin-react-function")).with(project(":kotlin-react-function"))
-    substitute(module("com.bnorm.react:kotlin-react-function-gradle")).with(project(":kotlin-react-function-gradle"))
-    substitute(module("com.bnorm.react:kotlin-react-function-plugin")).with(project(":kotlin-react-function-plugin"))
-  }
-}
+includeBuild("..")
