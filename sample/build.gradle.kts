897cfea1dc7d69fb7b6dafb5ff7a04f8678caca6
Brian Norman <brian@bnorm.com>
2021-07-18 13:04:13 -0500

    Update to the latest version of the wrappers

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  kotlin("js") version "1.5.10"
-  kotlin("plugin.serialization") version "1.5.10"
+  kotlin("js") version "1.5.20"
+  kotlin("plugin.serialization") version "1.5.20"
   id("com.bnorm.react.kotlin-react-function") version "0.5.1"
 }
 
@@ -10,18 +10,20 @@ repositories {
 
 kotlin {
   js(IR) {
-    browser {}
+    browser()
     binaries.executable()
   }
 }
 
 dependencies {
-  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.0")
-  implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
+  implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.1")
 
   implementation("com.bnorm.react:kotlin-react-function:0.5.1")
-  implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:17.0.2-pre.206-kotlin-1.5.10")
-  implementation("org.jetbrains.kotlin-wrappers:kotlin-styled:5.3.0-pre.206-kotlin-1.5.10")
+
+  implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:0.0.1-pre.218-kotlin-1.5.21"))
+  implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
+  implementation("org.jetbrains.kotlin-wrappers:kotlin-styled")
 
   implementation(npm("@reach/accordion", "^0.13.0"))
   implementation(npm("prop-types", "^15.6.2")) // Not sure why this is needed...
