a53e743745244a99a350c03f82d8f8ef040c0f7b
dvdandroid <dvdandroid@users.noreply.github.com>
2022-01-11 10:26:59 +0100

    Use legacy 'render' call

--- sample/src/main/kotlin/com/bnorm/react/index.kt
+++ sample/src/main/kotlin/com/bnorm/react/index.kt
@@ -3,16 +3,11 @@
 package com.bnorm.react
 
 import kotlinx.browser.document
-import react.buildElement
 import react.dom.render
 import styled.injectGlobal
 import com.bnorm.react.IndexStyles as styles
 
 fun main() {
   injectGlobal(styles.global)
-
-  val container = document.createElement("div")
-  document.body!!.appendChild(container)
-
-  render(buildElement { App() }, container)
+  render(document.getElementById("root")!!) { App() }
 }
